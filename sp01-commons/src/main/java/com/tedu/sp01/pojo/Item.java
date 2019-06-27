51d305a088d48435c6cb6bec2d2dd46bb931205a
Tedu-CGB <Tedu-CGB@Tedu-CGB-PC>
2019-06-27 17:16:26 +0800

    1

--- sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Item {
+public class Item {//
 	private Integer id;
 	private String name;
 	private Integer number;
